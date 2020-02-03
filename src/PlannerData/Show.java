3871902d2bc4b69f261827bb9b3fff21e6cfde19
Lars Giskes <lars.giskes@gmail.com>
2020-02-03 11:15:33 +0100

    Stage v1

--- src/PlannerData/Show.java
+++ src/PlannerData/Show.java
@@ -1,5 +1,10 @@
 package PlannerData;
 
+import Enumerators.Genres;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class Show {
 
 	private int expectedPopularity;
@@ -7,11 +12,64 @@ public class Show {
 	private LocalDateTime endTime;
 	private String name;
 	private String description;
-	private String genre;
+	private Stage stage;
+	private ArrayList<Genres> genre;
+	private ArrayList<Artist> artists;
+
+	public Show(int expectedPopularity, LocalDateTime beginTime, LocalDateTime endTime, String name, String description, Stage stage, ArrayList<Genres> genre, ArrayList<Artist> artists) {
+		this.expectedPopularity = expectedPopularity;
+		this.beginTime = beginTime;
+		this.endTime = endTime;
+		this.name = name;
+		this.description = description;
+		this.stage = stage;
+		this.genre = genre;
+		this.artists = artists;
+	}
+
+	public int getExpectedPopularity() {
+		return expectedPopularity;
+	}
+
+	public LocalDateTime getBeginTime() {
+		return beginTime;
+	}
+
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public Stage getStage() {
+		return stage;
+	}
+
+	public ArrayList<Genres> getGenre() {
+		return genre;
+	}
+
+	public ArrayList<Artist> getArtists() {
+		return artists;
+	}
 
 	public LocalDateTime getDuration() {
-		// TODO - implement Show.getDuration
-		throw new UnsupportedOperationException();
+		int minutes = this.endTime.getMinute() - this.beginTime.getMinute();
+		if (minutes < 0){
+			minutes += 60;
+		}
+
+		int hours = this.endTime.getHour() - this.beginTime.getHour();
+
+		
+
+		LocalDateTime duration =
 	}
 
 }
