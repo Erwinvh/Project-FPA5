37c54f6594db557b909560f63945292c30c2a7af
Erwin <erwinvanhunnik@gmail.com>
2020-02-03 12:00:24 +0100

    Gui beginsel

--- src/GUILogic/ScheduleTab.java
+++ src/GUILogic/ScheduleTab.java
@@ -1,9 +1,14 @@
 package GUILogic;
 
 import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class ScheduleTab {
     private Tab scheduleTab;
+    private TableView table = new TableView();
 
     public ScheduleTab() {
         this.scheduleTab = new Tab("Schedule");
@@ -11,5 +16,45 @@ public class ScheduleTab {
 
     public Tab getScheduleTab() {
         return scheduleTab;
+    }
+
+
+    public void layout1(){
+
+
+
+        HBox baseLayer = new HBox();
+        baseLayer.setSpacing(10);
+
+        table.setEditable(false);
+
+        TableColumn beginTimeCol = new TableColumn("Begin time");
+        beginTimeCol.setPrefWidth(100);
+        TableColumn endTimeCol = new TableColumn("End time");
+        endTimeCol.setPrefWidth(100);
+        TableColumn stageCol = new TableColumn("Stage");
+        stageCol.setPrefWidth(100);
+        TableColumn artistCol = new TableColumn("Artist");
+        artistCol.setPrefWidth(100);
+        TableColumn genreCol = new TableColumn("Genre");
+        genreCol.setPrefWidth(100);
+        TableColumn popularityCol = new TableColumn("Popularity");
+        popularityCol.setPrefWidth(100);
+
+        table.getColumns().addAll(beginTimeCol, endTimeCol, stageCol, artistCol, genreCol, popularityCol);
+
+        baseLayer.getChildren().add(table);
+        baseLayer.getChildren().add(new javafx.scene.control.TextField());
+
+        HBox Controls = new HBox();
+
+        Controls.getChildren().add(new Button("Add"));
+
+        VBox base = new VBox();
+
+        base.getChildren().add(baseLayer);
+        base.getChildren().add(Controls);
+
+        this.scheduleTab.setContent(table);
     }
 }
