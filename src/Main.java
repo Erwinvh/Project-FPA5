11a79b8fa5942a3c5dc6f0da18f68b4b7c0bb0f9
Rik <rik.vos01@gmail.com>
2020-02-03 13:38:37 +0100

    resources update

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import Enumerators.Genres;
 import GUILogic.GUI;
+import PlannerData.Artist;
+import PlannerData.Planner;
+import PlannerData.Show;
+import PlannerData.Stage;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
 
 import static javafx.application.Application.launch;
 
 public class Main {
     public static void main(String[] args) {
+
+        //test data
+        Planner planner = new Planner();
+        ArrayList<Artist> artists = new ArrayList<>();
+        artists.add(new Artist("Arne", Genres.BLUES,"hij zingt en speelt gitaar"));
+        Show show = new Show(LocalTime.now(),LocalTime.now(),new Stage(100,"een"),artists,100);
+        planner.addShow(show);
+        planner.savePlanner();
+        System.out.println(planner.toString());
+
+
         launch(GUI.class);
     }
 }
--- src/PlannerData/Stage.java
+++ src/PlannerData/Stage.java
@@ -1,6 +1,8 @@
 package PlannerData;
 
-public class Stage {
+import java.io.Serializable;
+
+public class Stage implements Serializable {
 
 	private int capacity;
 	private String name;
