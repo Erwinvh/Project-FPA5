18e5bf149bcc16a3041a7a95724682dd388d57c9
Erwin <erwinvanhunnik@gmail.com>
2020-02-03 21:18:14 +0100

    Update Pop-ups
    Alle drie de pop-ups zijn nu verbeterd met de diverse knoppen er in gezet (niet functioneel). De add menu is half af (geen controle element en geen dropdown of dergelijke voor de artiesten). de delete moet de informatie en functionele knoppen nog krijgen. De edit moet nog veel aan gedaan worden

--- src/GUILogic/ScheduleTab.java
+++ src/GUILogic/ScheduleTab.java
@@ -4,15 +4,21 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ScheduleTab {
     private Tab scheduleTab;
     private TableView table = new TableView();
+    private VBox description = new VBox();
+    private HBox controls = new HBox();
+    private Stage primaryStage;
 
-    public ScheduleTab() {
+    public ScheduleTab(Stage primaryStage) {
+        this.primaryStage = primaryStage;
         this.scheduleTab = new Tab("Schedule");
         layout1();
     }
@@ -21,11 +27,7 @@ public class ScheduleTab {
         return scheduleTab;
     }
 
-    public void layout1(){
-
-        HBox baseLayer = new HBox();
-        baseLayer.setSpacing(10);
-
+    public void table(){
         this.table.setEditable(false);
 
         TableColumn beginTimeCol = new TableColumn("Begin time");
@@ -43,32 +45,89 @@ public class ScheduleTab {
         this.table.setPrefWidth(875);
 
         this.table.getColumns().addAll(beginTimeCol, endTimeCol, stageCol, artistCol, genreCol, popularityCol);
+    }
 
-        VBox description = new VBox();
-
+    public void desciption(){
         Image baseImage = new Image("file:Resources/PersonImageBase.jpg");
         ImageView Artistpicture = new ImageView(baseImage);
         Artistpicture.setFitHeight(200);
         Artistpicture.setFitWidth(200);
-//        Artistpicture.setScaleY(0.5);
-        description.getChildren().add(Artistpicture);
+
         TextArea artistDescription = new TextArea("Description of artist 1");
         artistDescription.setEditable(false);
 
-        description.getChildren().add(artistDescription);
+        this.description.getChildren().add(Artistpicture);
+        this.description.getChildren().add(artistDescription);
+    }
+
+    public void layout1(){
+        HBox baseLayer = new HBox();
+        baseLayer.setSpacing(10);
+        table();
+        desciption();
 
         baseLayer.getChildren().add(this.table);
-        baseLayer.getChildren().add(description);
+        baseLayer.getChildren().add(this.description);
 
-        HBox Controls = new HBox();
+        VBox base = new VBox();
+        base.getChildren().add(baseLayer);
+        Controls();
+        base.getChildren().add(this.controls);
 
-        Button addButton = new Button("Add");
+        this.scheduleTab.setContent(base);
+    }
+
+    public void Controls(){
+       Button addButton = new Button("Add");
         addButton.setOnAction(event -> {
             Stage adderStage = new Stage();
+            adderStage.setWidth(400);
+            adderStage.setHeight(450);
+            adderStage.initOwner(this.primaryStage);
+            adderStage.initModality(Modality.WINDOW_MODAL);
 
-            Label addingNew = new Label("Add a new show");
+            BorderPane structure = new BorderPane();
 
-            Scene adderScene = new Scene(addingNew);
+            Label addingNew = new Label("what show do you want to add?");
+            structure.setTop(addingNew);
+            Label information = new Label("info dump");
+
+
+            VBox inputID = new VBox();
+            inputID.getChildren().add(new Label("Begin time:"));
+            inputID.getChildren().add(new Label("End time:"));
+            inputID.getChildren().add(new Label("Stage:"));
+            inputID.getChildren().add(new Label("Genre:"));
+            inputID.getChildren().add(new Label("popularity:"));
+            inputID.getChildren().add(new Label("Artists:"));
+            inputID.setSpacing(10);
+
+            VBox inputFields = new VBox();
+            TextField beginTime = new TextField();
+            TextField endTime = new TextField();
+            TextField stage = new TextField();
+            TextField genre = new TextField();
+            TextField popularity = new TextField();
+
+            inputFields.getChildren().add(beginTime);
+            inputFields.getChildren().add(endTime);
+            inputFields.getChildren().add(stage);
+            inputFields.getChildren().add(genre);
+            inputFields.getChildren().add(popularity);
+
+            HBox inputsystem = new HBox();
+            inputsystem.getChildren().add(inputID);
+            inputsystem.getChildren().add(inputFields);
+
+            structure.setCenter(inputsystem);
+            HBox choice = new HBox();
+            Button cancel = new Button("Cancel");
+            Button submit = new Button("Submit");
+            choice.getChildren().add(cancel);
+            choice.getChildren().add(submit);
+            structure.setBottom(choice);
+
+            Scene adderScene = new Scene(structure);
             adderStage.setScene(adderScene);
             adderStage.show();
 
@@ -76,10 +135,26 @@ public class ScheduleTab {
         Button editButton = new Button("Edit");
         editButton.setOnAction(event -> {
             Stage editStage = new Stage();
+            editStage.setWidth(250);
+            editStage.setHeight(250);
+            editStage.initOwner(this.primaryStage);
+            editStage.initModality(Modality.WINDOW_MODAL);
 
-            Label editingThis = new Label("Edit this show");
+            BorderPane structure = new BorderPane();
 
-            Scene editScene = new Scene(editingThis);
+            Label editingThis = new Label("Edit this show:");
+            structure.setTop(editingThis);
+            Label information = new Label("editable info dump");
+            structure.setCenter(information);
+
+            HBox choice = new HBox();
+            Button cancel = new Button("Cancel");
+            Button submit = new Button("Submit");
+            choice.getChildren().add(cancel);
+            choice.getChildren().add(submit);
+            structure.setBottom(choice);
+
+            Scene editScene = new Scene(structure);
             editStage.setScene(editScene);
             editStage.show();
 
@@ -87,25 +162,40 @@ public class ScheduleTab {
         Button deleteButton = new Button("Delete");
         deleteButton.setOnAction(event -> {
             Stage deleteStage = new Stage();
+            deleteStage.setWidth(250);
+            deleteStage.setHeight(250);
+            deleteStage.initOwner(this.primaryStage);
+            deleteStage.initModality(Modality.WINDOW_MODAL);
+            BorderPane structure = new BorderPane();
 
-            Label deleteThis = new Label("delete this show");
+            Label deleteThis = new Label("Are you sure you want to delete this show?");
+            structure.setTop(deleteThis);
+            Label information = new Label("Information...");
+            structure.setCenter(information);
 
-            Scene deleteScene = new Scene(deleteThis);
+            HBox choice = new HBox();
+
+            Button cancel = new Button("No");
+//            cancel.setOnAction(event -> {
+//                //cancel option
+//                    });
+            Button confirm = new Button("Yes");
+//            confirm.setOnAction(event -> {
+//                //delete file
+//            });
+            choice.getChildren().add(cancel);
+            choice.getChildren().add(confirm);
+
+            structure.setBottom(choice);
+            Scene deleteScene = new Scene(structure);
             deleteStage.setScene(deleteScene);
             deleteStage.show();
-
         });
 
-        Controls.getChildren().add(addButton);
-        Controls.getChildren().add(editButton);
-        Controls.getChildren().add(deleteButton);
-        Controls.setSpacing(20);
-
-        VBox base = new VBox();
-
-        base.getChildren().add(baseLayer);
-        base.getChildren().add(Controls);
-
-        this.scheduleTab.setContent(base);
+        this.controls.getChildren().add(addButton);
+        this.controls.getChildren().add(editButton);
+        this.controls.getChildren().add(deleteButton);
+        this.controls.setSpacing(20);
     }
+
 }
