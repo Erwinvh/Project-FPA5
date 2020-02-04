ae5cf2cfcd38526e2fd188f63431419f42a4c530
Erwin <erwinvanhunnik@gmail.com>
2020-02-04 12:04:02 +0100

    Pop-ups sluiting
    Pop-ups kunnen nu sluiten als je op cancel klikt

--- src/GUILogic/ScheduleTab.java
+++ src/GUILogic/ScheduleTab.java
@@ -16,11 +16,18 @@ public class ScheduleTab {
     private VBox description = new VBox();
     private HBox controls = new HBox();
     private Stage primaryStage;
+    private Stage popUp = new Stage();
+    private Button confirm = new Button("Confirm");
+    private Button cancel = new Button("Cancel");
 
     public ScheduleTab(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.scheduleTab = new Tab("Schedule");
         layout1();
+        this.popUp.setWidth(400);
+        this.popUp.setHeight(450);
+        this.popUp.initOwner(this.primaryStage);
+        this.popUp.initModality(Modality.WINDOW_MODAL);
     }
 
     public Tab getScheduleTab() {
@@ -65,7 +72,8 @@ public class ScheduleTab {
         baseLayer.setSpacing(10);
         table();
         desciption();
-
+        cancelsetup();
+        
         baseLayer.getChildren().add(this.table);
         baseLayer.getChildren().add(this.description);
 
@@ -77,125 +85,126 @@ public class ScheduleTab {
         this.scheduleTab.setContent(base);
     }
 
+    public void cancelsetup(){
+        this.cancel.setOnAction(event -> {
+            this.popUp.close();
+        });
+    }
+
     public void Controls(){
        Button addButton = new Button("Add");
         addButton.setOnAction(event -> {
-            Stage adderStage = new Stage();
-            adderStage.setWidth(400);
-            adderStage.setHeight(450);
-            adderStage.initOwner(this.primaryStage);
-            adderStage.initModality(Modality.WINDOW_MODAL);
-
-            BorderPane structure = new BorderPane();
-
-            Label addingNew = new Label("what show do you want to add?");
-            structure.setTop(addingNew);
-            Label information = new Label("info dump");
-
-
-            VBox inputID = new VBox();
-            inputID.getChildren().add(new Label("Begin time:"));
-            inputID.getChildren().add(new Label("End time:"));
-            inputID.getChildren().add(new Label("Stage:"));
-            inputID.getChildren().add(new Label("Genre:"));
-            inputID.getChildren().add(new Label("popularity:"));
-            inputID.getChildren().add(new Label("Artists:"));
-            inputID.setSpacing(10);
-
-            VBox inputFields = new VBox();
-            TextField beginTime = new TextField();
-            TextField endTime = new TextField();
-            TextField stage = new TextField();
-            TextField genre = new TextField();
-            TextField popularity = new TextField();
-
-            inputFields.getChildren().add(beginTime);
-            inputFields.getChildren().add(endTime);
-            inputFields.getChildren().add(stage);
-            inputFields.getChildren().add(genre);
-            inputFields.getChildren().add(popularity);
-
-            HBox inputsystem = new HBox();
-            inputsystem.getChildren().add(inputID);
-            inputsystem.getChildren().add(inputFields);
-
-            structure.setCenter(inputsystem);
-            HBox choice = new HBox();
-            Button cancel = new Button("Cancel");
-            Button submit = new Button("Submit");
-            choice.getChildren().add(cancel);
-            choice.getChildren().add(submit);
-            structure.setBottom(choice);
-
-            Scene adderScene = new Scene(structure);
-            adderStage.setScene(adderScene);
-            adderStage.show();
-
+            additionWindow();
         });
         Button editButton = new Button("Edit");
         editButton.setOnAction(event -> {
-            Stage editStage = new Stage();
-            editStage.setWidth(250);
-            editStage.setHeight(250);
-            editStage.initOwner(this.primaryStage);
-            editStage.initModality(Modality.WINDOW_MODAL);
-
-            BorderPane structure = new BorderPane();
-
-            Label editingThis = new Label("Edit this show:");
-            structure.setTop(editingThis);
-            Label information = new Label("editable info dump");
-            structure.setCenter(information);
-
-            HBox choice = new HBox();
-            Button cancel = new Button("Cancel");
-            Button submit = new Button("Submit");
-            choice.getChildren().add(cancel);
-            choice.getChildren().add(submit);
-            structure.setBottom(choice);
-
-            Scene editScene = new Scene(structure);
-            editStage.setScene(editScene);
-            editStage.show();
-
+            editoryWindow();
         });
         Button deleteButton = new Button("Delete");
         deleteButton.setOnAction(event -> {
-            Stage deleteStage = new Stage();
-            deleteStage.setWidth(250);
-            deleteStage.setHeight(250);
-            deleteStage.initOwner(this.primaryStage);
-            deleteStage.initModality(Modality.WINDOW_MODAL);
-            BorderPane structure = new BorderPane();
-
-            Label deleteThis = new Label("Are you sure you want to delete this show?");
-            structure.setTop(deleteThis);
-            Label information = new Label("Information...");
-            structure.setCenter(information);
-
-            HBox choice = new HBox();
-
-            Button cancel = new Button("No");
-//            cancel.setOnAction(event -> {
-//                //cancel option
-//                    });
-            Button confirm = new Button("Yes");
-//            confirm.setOnAction(event -> {
-//                //delete file
-//            });
-            choice.getChildren().add(cancel);
-            choice.getChildren().add(confirm);
-
-            structure.setBottom(choice);
-            Scene deleteScene = new Scene(structure);
-            deleteStage.setScene(deleteScene);
-            deleteStage.show();
+            deletionWindow();
         });
 
         this.controls.getChildren().add(addButton);
         this.controls.getChildren().add(editButton);
         this.controls.getChildren().add(deleteButton);
         this.controls.setSpacing(20);
+    }
+
+    public void additionWindow(){
+        BorderPane structure = new BorderPane();
+
+        Label addingNew = new Label("what show do you want to add?");
+        structure.setTop(addingNew);
+        Label information = new Label("info dump");
+
+        VBox inputID = new VBox();
+        inputID.getChildren().add(new Label("Begin time:"));
+        inputID.getChildren().add(new Label("End time:"));
+        inputID.getChildren().add(new Label("Stage:"));
+        inputID.getChildren().add(new Label("Genre:"));
+        inputID.getChildren().add(new Label("popularity:"));
+        inputID.getChildren().add(new Label("Artists:"));
+        inputID.setSpacing(10);
+
+        VBox inputFields = new VBox();
+        TextField beginTime = new TextField();
+        TextField endTime = new TextField();
+        TextField stage = new TextField();
+        TextField genre = new TextField();
+        TextField popularity = new TextField();
+
+        inputFields.getChildren().add(beginTime);
+        inputFields.getChildren().add(endTime);
+        inputFields.getChildren().add(stage);
+        inputFields.getChildren().add(genre);
+        inputFields.getChildren().add(popularity);
+
+        HBox inputsystem = new HBox();
+        inputsystem.getChildren().add(inputID);
+        inputsystem.getChildren().add(inputFields);
+
+        structure.setCenter(inputsystem);
+        HBox choice = new HBox();
+        Button submit = new Button("Submit");
+        submit.setOnAction(event -> {
+            this.popUp.close();
+        });
+
+        choice.getChildren().add(this.cancel);
+        choice.getChildren().add(submit);
+        structure.setBottom(choice);
+
+        Scene adderScene = new Scene(structure);
+        this.popUp.setScene(adderScene);
+        this.popUp.show();
+    }
+
+    public void editoryWindow(){
+        BorderPane structure = new BorderPane();
+
+        Label editingThis = new Label("Edit this show:");
+        structure.setTop(editingThis);
+        Label information = new Label("editable info dump");
+        structure.setCenter(information);
+
+        HBox choice = new HBox();
+
+        Button submit = new Button("Submit");
+        submit.setOnAction(event -> {
+            this.popUp.close();
+        });
+
+        choice.getChildren().add(this.cancel);
+        choice.getChildren().add(submit);
+        structure.setBottom(choice);
+
+        Scene editScene = new Scene(structure);
+        this.popUp.setScene(editScene);
+        this.popUp.show();
+    }
+
+    public void deletionWindow(){
+        BorderPane structure = new BorderPane();
+
+        Label deleteThis = new Label("Are you sure you want to delete this show?");
+        structure.setTop(deleteThis);
+        Label information = new Label("Information...");
+        structure.setCenter(information);
+
+        HBox choice = new HBox();
+
+        Button confirm = new Button("Yes");
+        confirm.setOnAction(event -> {
+            this.popUp.close();
+        });
+        choice.getChildren().add(this.cancel);
+        choice.getChildren().add(confirm);
+
+        structure.setBottom(choice);
+        Scene deleteScene = new Scene(structure);
+        this.popUp.setScene(deleteScene);
+        this.popUp.show();
     }
 
 }
