54d7687be9ffbe312e12cd29708d309fb7e4e335
Erwin <erwinvanhunnik@gmail.com>
2020-03-06 11:31:06 +0100

    [Feature] SuperGenre added to genre
    SuperGenre has been added to support NPC movement and prediction

--- src/Enumerators/Genres.java
+++ src/Enumerators/Genres.java
@@ -12,37 +12,43 @@ package Enumerators;
 //    }
 
 public enum Genres {
-    ALTERNATIVE_ROCK("Alternative rock"),
-    BLUES("Blues"),
-    BLUES_ROCK("Blues rock"),
-    CLASSICAL("Classical"),
-    COUNTRY("Country"),
-    DANCE("Dance"),
-    ELECTRONIC("Electronic"),
-    EMO_ROCK("Emo rock"),
-    FOLK("Folk"),
-    HIP_HOP("Hip hop"),
-    HOUSE("House"),
-    INDIE_POP("Indie pop"),
-    JAZZ("Jazz"),
-    K_POP("K-pop"),
-    METAL("Metal"),
-    NIGHTCORE("Nightcore"),
-    POP("Pop"),
-    PUNK_ROCK("Punk rock"),
-    RAP("Rap"),
-    REGGEA("Reggea"),
-    ROCK("Rock"),
-    SOUL("Soul");
+    ALTERNATIVE_ROCK("Alternative rock", "Metal"),
+    BLUES("Blues","Classic"),
+    BLUES_ROCK("Blues rock", "Metal"),
+    CLASSICAL("Classical", "Classic"),
+    COUNTRY("Country","Country"),
+    DANCE("Dance", "Electro"),
+    ELECTRONIC("Electronic", "Electro"),
+    EMO_ROCK("Emo rock","Metal"),
+    FOLK("Folk", "Country"),
+    HIP_HOP("Hip hop", "Rap"),
+    HOUSE("House", "Electro"),
+    INDIE_POP("Indie pop", "Pop"),
+    JAZZ("Jazz", "Classic"),
+    K_POP("K-pop", "Pop"),
+    METAL("Metal", "Metal"),
+    NIGHTCORE("Nightcore", "Electro"),
+    POP("Pop", "Pop"),
+    PUNK_ROCK("Punk rock", "Metal"),
+    RAP("Rap", "Rap"),
+    REGGEA("Reggea", "Rap"),
+    ROCK("Rock", "Metal"),
+    SOUL("Soul", "Classic");
 
     private String fancyName;
+    private String SuperGenre;
 
-    Genres(String fancyName) {
+    Genres(String fancyName, String SuperGenre) {
         this.fancyName = fancyName;
+        this.SuperGenre = SuperGenre;
     }
 
     public String getFancyName(){
         return this.fancyName;
+    }
+
+    public String getSuperGenre() {
+        return SuperGenre;
     }
 
     public static Genres getGenre(String name){
