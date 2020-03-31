3fb89231b272893c3cb46dc39766d478cb684af7
Lars Giskes <lars.giskes@gmail.com>
2020-03-31 17:23:22 +0200

    [Refactor] Made PopularityTracker more efficient

--- src/GUILogic/PopularityTracker.java
+++ src/GUILogic/PopularityTracker.java
@@ -3,34 +3,34 @@ package GUILogic;
 import PlannerData.Show;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A class that counts the amount of people present at shows
  */
 
 public class PopularityTracker {
+    HashMap<Show, Integer> peopleAtShows;
     private ArrayList<Show> activeShows;
-    private ArrayList<Integer> amountAtShows;
 
     public PopularityTracker(){
+        peopleAtShows = new HashMap<>();
         activeShows = DataController.getInstance().getActiveShows();
-        amountAtShows = new ArrayList<>();
         for(Show show : activeShows){
-            amountAtShows.add(0);
+            peopleAtShows.put(show, 0);
         }
     }
 
     /**
-     * Looks if there is room for one extra visitor in a show
+     * Looks if there is room for one extra visitor in a show and adds one to the amount people to the show
      * @param show the show the visitor wants to go to
      * @return true if there is room, false if the show is full
      */
     public boolean canGoToShow(Show show){
-        for(Show activeShow : activeShows){
+        for(Show activeShow : peopleAtShows.keySet()){
             if(show == activeShow){
-                int showIndex = activeShows.indexOf(activeShow);
-                if(show.getStage().getCapacity() >= amountAtShows.get(showIndex)-1){
-                    amountAtShows.set(showIndex,amountAtShows.get(showIndex) + 1);
+                if(show.getStage().getCapacity() >= peopleAtShows.get(show) + 1){
+                    peopleAtShows.put(show, peopleAtShows.get(show) + 1);
                     return true;
                 }
             }
