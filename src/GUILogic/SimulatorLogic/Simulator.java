9696f463cdc20055b0ee0a2023157d0485ec016a
Lars Giskes <lars.giskes@gmail.com>
2020-03-31 17:22:47 +0200

    Auto stash before merge of "refactor" and "origin/refactor"

--- src/GUILogic/SimulatorLogic/Simulator.java
+++ src/GUILogic/SimulatorLogic/Simulator.java
@@ -77,6 +77,22 @@ public class Simulator {
 
         }
 
+        try{
+            MediaPlayer background = new MediaPlayer(new Media(getClass().getResource("/soundEffects/PawPrints.mp3").toString()));
+            background.setVolume(0.5);
+            background.setAutoPlay(true);
+        }catch (Exception e){
+
+        }
+
+        try{
+            MediaPlayer background = new MediaPlayer(new Media(getClass().getResource("/soundEffects/PawPrints.mp3").toString()));
+            background.setVolume(0.5);
+            background.setAutoPlay(true);
+        }catch (Exception e){
+
+        }
+
         ArrayList<Show> sortedShowList = DataController.getInstance().getPlanner().getShows();
         sortedShowList.sort(Show::compareToTime);
 
